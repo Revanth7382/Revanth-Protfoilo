f266b9cf1ba18d20baead309615aef278e459541
Revanth7382 <127931760+Revanth7382@users.noreply.github.com>
2024-03-24 23:18:42 +0530

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,7 @@
 import ResumeImg from '../assets/resume.jpg'
 export default function Resume(){
     const config={
-        Resume:'https://drive.google.com/file/d/1GGWjtaSNTnQgkeJrY5Ep3TBhg4CfhywN/view?pli=1'
+        Resume:'https://drive.google.com/file/d/1GGWjtaSNTnQgkeJrY5Ep3TBhg4CfhywN/view?usp=sharing'
     }
     return <section id='resume' className='flex flex-col md:flex-row bg-secondary px-5'>
         <div className='py-5 md:w-1/2 flex justify-center'>
